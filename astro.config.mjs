9b093cbeff1632e7f36749c01be7ac570995dad9
Krzysztof Begiedza <8686134+kbegiedza@users.noreply.github.com>
2024-04-28 12:04:02 +0000

    update directories

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,31 +4,41 @@ import starlight from '@astrojs/starlight';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [	starlight({
-    title: 'notes.kbegiedza.eu',
-    social: {
-      github: 'https://github.com/kbegiedza/notes.kbegiedza.eu'
-    })
-    // sidebar: [{
-      //   label: 'Languages',
-      //   autogenerate: {
-        //     directory: 'languages'
-        //   }
-        //   items: [
-    //   	// Each item here is one entry in the navigation menu.
-    //   	{ label: 'Example Guide', link: '/guides/example/' },
-    //   ],
-    // }, {
-      //   label: 'Databases',
-      //   autogenerate: {
-        //     directory: 'databases'
-        //   }
-        // }, {
-    //   label: 'Reference',
-    //   autogenerate: {
-    //     directory: 'reference'
-    //   }
-    // }]
-  ]
-});
+export default defineConfig(
+  {
+    integrations: [
+      starlight(
+      {
+        title: 'notes.kbegiedza.eu',
+        social: {
+          github: 'https://github.com/kbegiedza/notes.kbegiedza.eu'
+        },
+        sidebar: [
+          {
+            label: 'Software Engineer',
+            collapsed: true,
+            autogenerate:
+            {
+              directory: '00-software-engineer'
+            }
+          },
+          {
+            label: '.NET Specialist',
+            collapsed: true,
+            autogenerate:
+            {
+              directory: '01-dotnet-engineer'
+            }
+          },
+          {
+            label: 'Reference',
+            autogenerate: { directory: 'reference'}
+              // items: [
+              //   // Each item here is one entry in the navigation menu.
+              //   { label: 'Example Guide', link: '/guides/example/' },
+              // ],
+          },
+        ]
+      })
+    ]
+  });
