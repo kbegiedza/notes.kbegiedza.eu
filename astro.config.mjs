5f76bf9617a80412b6b308f0df3416ed501b69a8
Krzysztof Begiedza <8686134+kbegiedza@users.noreply.github.com>
2024-05-05 09:53:36 +0000

    Switch to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,52 +1,52 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig(
-  {
-    integrations: [
-      starlight(
-      {
-        social: {
-          github: 'https://github.com/kbegiedza/notes.kbegiedza.eu'
-        },
-        title: 'notes.kbegiedza.eu',
-        locales: {
-          root: {
-            label: 'English',
-            lang: 'en-GB'
-          },
-          pl: {
-            label: 'Polski',
-            lang: 'pl-PL'
-          },
-        },
-        sidebar: [
-          {
-            label: 'Software Engineer',
-            collapsed: true,
-            autogenerate:
-            {
-              directory: '00-software-engineer'
-            }
-          },
-          {
-            label: '.NET Engineer',
-            collapsed: true,
-            autogenerate:
-            {
-              directory: '01-dotnet-engineer'
-            }
-          },
-          {
-            label: 'Reference',
-            autogenerate: { directory: 'reference'}
-              // items: [
-              //   // Each item here is one entry in the navigation menu.
-              //   { label: 'Example Guide', link: '/guides/example/' },
-              // ],
-          },
-        ]
-      })
+export default defineConfig({
+  integrations: [starlight({
+    social: {
+      github: 'https://github.com/kbegiedza/notes.kbegiedza.eu'
+    },
+    title: 'notes.kbegiedza.eu',
+    locales: {
+      root: {
+        label: 'English',
+        lang: 'en-GB'
+      },
+      pl: {
+        label: 'Polski',
+        lang: 'pl-PL'
+      }
+    },
+    sidebar: [{
+      label: 'Software Engineer',
+      collapsed: true,
+      autogenerate: {
+        directory: '00-software-engineer'
+      }
+    }, {
+      label: '.NET Engineer',
+      collapsed: true,
+      autogenerate: {
+        directory: '01-dotnet-engineer'
+      }
+    }, {
+      label: 'Reference',
+      autogenerate: {
+        directory: 'reference'
+      }
+      // items: [
+      //   // Each item here is one entry in the navigation menu.
+      //   { label: 'Example Guide', link: '/guides/example/' },
+      // ],
+    }],
+    customCss: [
+      './src/styles/tailwind.css',
     ]
-  });
+  }),
+  tailwind({
+    applyBaseStyles: false,
+  })]
+});
