4a523f7e9e2a78620ec4df2d952afd8f4ee81b3f
Krzysztof Begiedza <8686134+kbegiedza@users.noreply.github.com>
2024-05-06 18:52:59 +0000

    WIP question component

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,38 +1,52 @@
-import { Card } from "@astrojs/starlight/components";
-
 interface IQuestionOption {
-  content: string;
-  isCorrect: boolean;
+    content: string;
+    isCorrect: boolean;
 }
 
 interface IQuestionProps {
-  question: string;
-  questionId: string;
-  options: IQuestionOption[];
+    id: string;
+    question: string;
+    options: IQuestionOption[];
 }
 
 const Question = (props: IQuestionProps) => {
-  const questionContent = props.question;
-  const
+    const questionContent = props.question;
 
-  return (
-    <Card title={questionContent}>
-      <ul>
-        {options.map((option, index) => (
-          <li>
-            <input
-              type={isSingleChoice ? "radio" : "checkbox"}
-              name="quiz"
-              id={`${questionId}-option-${index}`}
-            />
-            <label for={`${questionId}-option-${index}`}>
-              {option.content}
-            </label>
-          </li>
-        ))}
-      </ul>
-    </Card>
-  );
+    const isSingleChoice =
+        props.options.filter((option) => option.isCorrect).length === 1;
+
+    return (
+        <div>
+            <p>{questionContent}</p>
+            <ul role="list" className="divide-y divide-gray-200">
+                {props.options.map((option, index) => (
+                    <li key={index} className="px-4 py-4 sm:px-0">
+                        <input
+                            id={`${props.id}-${index}`}
+                            name={`${props.id}`}
+                            type={isSingleChoice ? "radio" : "checkbox"}
+                            onChange={(element) => {
+                                const parent =
+                                    element.currentTarget.parentElement;
+                                if (parent == null) {
+                                    return;
+                                }
+
+                                if (option.isCorrect) {
+                                    parent.classList.add("bg-green-200");
+                                } else {
+                                    parent.classList.add("bg-red-200");
+                                }
+                            }}
+                        />
+                        <label htmlFor={`${props.id}-${index}`}>
+                            {option.content}
+                        </label>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default Question;
